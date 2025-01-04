4479a91b078b1165a287ae6b82cf36710d6b4508
Andrew Thomas <Andr3w.l.thomas@gmail.com>
2025-01-04 17:21:43 -0500

    header links to homepage

--- src/components/MainLayout/index.tsx
+++ src/components/MainLayout/index.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from "react";
 import "./styles.css";
 import { Toaster } from "react-hot-toast";
+import { Link } from "react-router";
 
 type TProps = { children: ReactNode; victory?: boolean };
 
@@ -10,7 +11,11 @@ function MainLayout({ children, victory }: TProps) {
       <Toaster />
       <header>
         {victory && <div className="emoji">🎉</div>}
-        <h1 className="page-hero">Adieu</h1>
+        <h1 className="page-hero">
+          <Link to="/" style={{ textDecoration: "none", color: "black" }}>
+            Adieu
+          </Link>
+        </h1>
         {victory && <div className="emoji">🎉</div>}
       </header>
       {children}
